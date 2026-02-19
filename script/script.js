b63318c4c9a4f96da58aa6bcd97f9f8da883627c
Eduardo Pozenatto <235704056+eduardopozenatto@users.noreply.github.com>
2026-02-19 11:25:12 -0300

    ajuste dos botões de login e cadastro que estavam se afastando ao clicar, adicionando borda à classe .button

--- script/script.js
+++ script/script.js
@@ -1,14 +1,20 @@
+// constantes com os botões de login, cadastro superiores e "esqueceu a senha?"
+
 const logins = document.querySelectorAll(".login-link");
 const signups = document.querySelectorAll(".signup-link");
 const recovery_password = document.querySelector(".forgot-password");
 
+// constante com o botão de voltar para o login (recovery password page)
 const back_to_login = document.getElementById("back-title");
 
+// constantes com os elementos de cada formulário
 const login_form = document.querySelector(".login-form");
 const signup_form = document.querySelector(".signup-form");
 const recovery_form = document.querySelector(".recovery-form");
 
-
+// Para cada botão com a classe respectiva dos botões, 
+// executa a função que adiciona um evento 'click' para 
+// mostrar/esconder os formulários
 logins.forEach(function(login) {
     login.addEventListener('click', function(event) {
         login_form.style.display = 'flex';
